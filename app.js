8348b995a39ab73d2d7fdbe88e779bc7a6282060
max5961 <mmorrissey5961@gmail.com>
2023-03-25 20:18:56 -0400

    Very rough draft

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const newAlbum = document.querySelector('.newAlbum');
+const albumsContainer = document.querySelector('.albumsContainer');
+const albumForm = document.querySelector('.albumForm');
+
+
+const submit = document.querySelector('.submit');
+const title = document.getElementById('title');
+const band = document.getElementById('band');
+const year = document.getElementById('year');
+const runtime = document.getElementById('runtime');
+const youtube = document.getElementById('youtube');
+
+
 let collection = [];
 
 function Album(title, band, year, runtime, youtubeLink){
@@ -12,6 +25,27 @@ function addToCollection(album){
     collection.push(album);
 }
 
-const nigga = new Album(1,2,3,4,5);
-addToCollection(nigga);
-console.log(collection);
+newAlbum.addEventListener('click', () => {
+    albumForm.style.visibility = 'visible';
+})
+
+function albumDisplay(album){
+    let albumGui = document.createElement('div');
+    albumGui.classList.add('album');
+    albumsContainer.appendChild(albumGui);
+
+    albumGui.textContent = 'test';
+}
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    albumForm.style.visibility = 'hidden';
+    let album = new Album(title.value, band.value, year.value, runtime.value, youtube.value)
+
+    addToCollection(album);
+    console.log(collection);
+
+    albumDisplay(album);
+    
+})
+
