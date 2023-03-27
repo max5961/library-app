76041e6ea30e6e54d1de67fb96e76eadc237d8a5
max5961 <mmorrissey5961@gmail.com>
2023-03-27 13:34:02 -0400

    Basic layout, some JS

--- app.js
+++ app.js
@@ -1,51 +1,19 @@
 const newAlbum = document.querySelector('.newAlbum');
-const albumsContainer = document.querySelector('.albumsContainer');
-const albumForm = document.querySelector('.albumForm');
+const addNewContent = document.querySelector('.addNewContent');
+const exitNew = document.querySelector('.exitNew');
+const collection = document.querySelector('.collection');
 
+let submit = false;
+let exitBool = false; //need to create exitNew button.
 
-const submit = document.querySelector('.submit');
-const title = document.getElementById('title');
-const band = document.getElementById('band');
-const year = document.getElementById('year');
-const runtime = document.getElementById('runtime');
-const youtube = document.getElementById('youtube');
-
-
-let collection = [];
-
-function Album(title, band, year, runtime, youtubeLink){
-    this.title = title;
-    this.band = band;
-    this.year = year;
-    this.runtime = runtime;
-    this.youtubeLink = youtubeLink;
+function toggleVisibility(){
+    if(submit || exitBool){
+        addNewContent.style.visibility = 'hidden';
+        collection.style.visibility = 'visible'
+    } else {
+        addNewContent.style.visibility = 'visible';
+        collection.style.visibility = 'hidden';
+    }
 }
 
-function addToCollection(album){
-    collection.push(album);
-}
-
-newAlbum.addEventListener('click', () => {
-    albumForm.style.visibility = 'visible';
-})
-
-function albumDisplay(album){
-    let albumGui = document.createElement('div');
-    albumGui.classList.add('album');
-    albumsContainer.appendChild(albumGui);
-
-    albumGui.textContent = 'test';
-}
-
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    albumForm.style.visibility = 'hidden';
-    let album = new Album(title.value, band.value, year.value, runtime.value, youtube.value)
-
-    addToCollection(album);
-    console.log(collection);
-
-    albumDisplay(album);
-    
-})
-
+newAlbum.addEventListener('click', toggleVisibility);
